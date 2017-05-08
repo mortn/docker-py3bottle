938a6fabbc67feb409f6874966b30cb5c3e927a6
Morten Abildgaard <mortn@users.noreply.github.com>
2017-05-08 13:05:14 +0200

    Create myotpsecrets.py

---
+++ app/myotpsecrets.py
@@ -0,0 +1,8 @@
+ttp_user = 'admin'
+http_pass = 'admin'
+codes = {
+	'account1': 'pefjehegNusherewSunaumIcwoafIfyi',
+	'account2': 'memJarrIfomWeykvajLyutIkJeafcoyt',
+	'account3': 'rieshjaynEgDoipEjkecPopHiWighath',
+}
+
