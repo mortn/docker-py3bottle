97a967fd7a6f058fe097a4e47e4e18d7b3ad0c20
Morten Abildgaard <mortn@users.noreply.github.com>
2017-05-21 14:14:48 +0200

    Delete myotpsecrets.py

--- app/myotpsecrets.py
+++
@@ -1,9 +0,0 @@
-# replace with your actual secret stuff ;-)
-http_user = 'admin'
-http_pass = 'admin'
-codes = {
-	'account1': 'pefjehegNusherewSunaumIcwoafIfyi',
-	'account2': 'memJarrIfomWeykvajLyutIkJeafcoyt',
-	'account3': 'rieshjaynEgDoipEjkecPopHiWighath',
-}
-
